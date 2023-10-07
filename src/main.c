22e37a0d5f2daf027b484027d04eadb3bba8a137
P34RL <undeadgamer279@gmail.com>
2023-10-07 19:52:33 -0400

    main function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,45 @@
+#include <conio.h>
+#include <ctype.h>
 #include <stdio.h>
+#include "task.h"
+#include "ui.h"
+
+#define INSTRUCTIONS "'A'dd 'D'elete 'T'oggle complete 'Q'uit\n"
+
+enum options {
+    ADD = 'a',
+    DELETE = 'd',
+    TOGGLE = 't',
+    QUIT = 'q',
+};
 
 int main() {
-    printf_s("Hello, World!");
-    return 0;
+    addTask("Learn C");
+
+    cls();
+    while (true) {
+        printf_s(INSTRUCTIONS);
+        viewTasks();
+
+        char ch = tolower(_getch());  // Wait for a key press
+
+        cls();
+
+        switch (ch) {
+            case QUIT:
+                return 0;
+            case ADD:
+                newTodo();
+                break;
+            case DELETE:
+                removeTodo();
+                break;
+            case TOGGLE:
+                toggleCompleted();
+                break;
+            default:
+                printf_s("'%c' is not an option\n", ch);
+                break;
+        }
+    }
 }
